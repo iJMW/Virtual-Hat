7a1d311aa706949cf43548fb84964d61b7f25fb2
Logan31and <loganbland18@yahoo.com>
2022-04-17 16:33:12 -0400

    Added cart and order management page

---
+++ ISYS366Project_Solution/ISYS366Project/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace ISYS366Project
+{
+    public class Order
+    {
+
+        public int Order_Id { get; set; }
+
+        public DateTime Date_Ordered { get; set; }
+
+        public float Total { get; set; }
+
+        public String Address { get; set; }
+
+        public String Placed_By { get; set; }
+
+        public int Merchandise_Id { get; set; }
+
+        public String Display_Active { get; set; }
+    }
+}
