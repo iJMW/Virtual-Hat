f2ef541d4ea16d7f89b7c5bab5c3a42138a80f84
Jalen Wayt <jalen.wayt@gmail.com>
2022-04-20 12:06:13 -0400

    Added initial setup for database structure

---
+++ Setup-4_17_22.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE ecommerce;
+
+USE ecommerce;
+
+CREATE TABLE Merchandise (
+	Merchandise_Id int NOT NULL AUTO_INCREMENT,
+    Merchandise_Name varchar(100) NOT NULL,
+    Price float,
+    Date_Added Date,
+    Brand varchar(100),
+    Display_Active varchar(1) NOT NULL,
+    PRIMARY KEY (Merchandise_Id)
+);
+
+CREATE TABLE Users (
+	Username varchar(100) BINARY NOT NULL,
+    User_Password varchar(100) BINARY NOT NULL,
+	Email varchar(100) BINARY NOT NULL,
+    First_Name varchar(100),
+    Last_Name varchar(100),
+    is_admin varchar(1),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Orders (
+	Order_Id int NOT NULL auto_increment,
+    Date_Ordered DateTime NOT NULL,
+    Total float NOT NULL,
+	Address varchar(100) NOT NULL,
+    Placed_By varchar(100) BINARY NOT NULL,
+    Merchandise_Id int NOT NULL,
+    Display_Active varchar(1) NOT NULL,
+    PRIMARY KEY (Order_Id),
+    FOREIGN KEY (Placed_By) REFERENCES Users(username),
+    FOREIGN KEY (Merchandise_Id) REFERENCES Merchandise(Merchandise_Id)
+);
+
+CREATE TABLE Reviews (
+	Review_Id int NOT NULL AUTO_INCREMENT,
+    Merchandise_Id int,
+    Reviewed_By varchar(100) BINARY,
+    Rating int,
+    User_Comment varchar(250),
+    PRIMARY KEY (Review_Id),
+    FOREIGN KEY (Merchandise_Id) REFERENCES Merchandise(Merchandise_Id),
+    FOREIGN KEY (Reviewed_By) REFERENCES Users(username)
+);
