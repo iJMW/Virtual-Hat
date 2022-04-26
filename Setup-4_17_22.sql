c9577f417a8ed99d5c61db7df20dd2a78d28ea80
Logan31and <loganbland18@yahoo.com>
2022-04-25 20:06:18 -0400

    Updated the intial setup SQL file to store initial merch and admin user

--- Setup-4_17_22.sql
+++ Setup-4_17_22.sql
@@ -34,3 +34,14 @@ CREATE TABLE Orders (
     FOREIGN KEY (Placed_By) REFERENCES Users(username),
     FOREIGN KEY (Merchandise_Id) REFERENCES Merchandise(Merchandise_Id)
 );
+
+INSERT 
+INTO Merchandise (Merchandise_Id, Merchandise_Name, Price, Date_Added, Brand, Display_Active)
+VALUES 	(1, "Big Hat", 100.00, STR_TO_DATE('04-06-2022', '%m-%d-%Y'), "JhnBrnd", "y"),
+		(2, "Cowboy Hat", 200.56, STR_TO_DATE('04-17-2022', '%m-%d-%Y'), "Rootin' Tootin'", "y"),
+        (3, "Magic Hat", 56.78,  STR_TO_DATE('04-17-2022', '%m-%d-%Y'), "Magic INC", "y"),
+        (4, "Spinner", 1.01, STR_TO_DATE('04-17-2022', '%m-%d-%Y'), "Spin", "y");
+
+INSERT 
+INTO Users (Username, User_Password, Email, First_Name, Last_Name, Is_Admin)
+VALUES 	("admin_user", "7uDRLhEubbt4eWnJqwjmjFzcDhBCjzGu7nXaKQz/iRE=", "admin@email.com", "admin", "admin", "y");
